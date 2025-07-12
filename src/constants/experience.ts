d63235658810891390e440523569148e5b81c032
Talha Mujahid <htmujahid@gmail.com>
2025-07-12 22:18:22 +0500

    feat: ui tweaks

---
+++ src/constants/experience.ts
@@ -0,0 +1,54 @@
+
+export const EXPERIENCES = [
+  {
+    company: "Devfluencers",
+    position: "React Engineer",
+    duration: "September 2023 — Present",
+    location: "Remote",
+    current: true,
+    description: [
+      "Developed AI based autobiography writer",
+      "Modularized code by introducing monorepo",
+      "Used supabase for auth, db, and realtime notifications",
+      "Open AI integration for ai response"
+    ],
+    technologies: ["React JS", "Next JS", "Supabase", "Postgres"],
+  },
+  {
+    company: "Invogue Solutions",
+    position: "Full Stack Developer",
+    duration: "March 2022 — August 2023",
+    location: "Islamabad, Pakistan",
+    description: [
+      "Developed SAAS applications on principle of multitenancy",
+      "Optimized the database structure for faster queries",
+      "Implemented the role and permission base authorization",
+      "Introduced the best practices of clean code"
+    ],
+    technologies: ["Laravel", "Vue JS", "Inertia JS", "MySQL"],
+  },
+  {
+    company: "Civica",
+    position: "Frontend Developer",
+    duration: "September 2022 — January 2023",
+    location: "Remote",
+    description: [
+      "Implemented frontend of enterprise level ecommerce plus real estate application from scratch",
+      "Used redux js for app wide state management",
+      "Developed using TailwindCSS as css framework"
+    ],
+    technologies: ["React JS", "Next JS"],
+  },
+  {
+    company: "Freelance",
+    position: "Web Developer",
+    duration: "January 2022 — Present",
+    location: "Remote",
+    description: [
+      "Delivered custom web solutions for small businesses and entrepreneurs",
+      "Provided ongoing maintenance and support for client websites",
+      "Developed strong client relationships resulting in 90% repeat business rate",
+    ],
+    technologies: ["React JS", "Next JS", "Tailwind CSS", "Supabase", "Postgres"],
+  },
+]
---
+++ src/constants/socials.ts
@@ -0,0 +1,28 @@
+import { Github, Linkedin, Mail, Twitter } from "lucide-astro";
+
+export const SOCIALS = [
+  {
+    name: "Email",
+    href: "mailto:htmujahid@gmail.com",
+    icon: Mail,
+    linkTitle: "htmujahid@gmail.com",
+  },
+  {
+    name: "LinkedIn",
+    href: "https://linkedin.com/in/htmujahid",
+    icon: Linkedin,
+    linkTitle: "linkedin.com/in/htmujahid",
+  },
+  {
+    name: "GitHub",
+    href: "https://github.com/htmujahid",
+    icon: Github,
+    linkTitle: "github.com/htmujahid",
+  },
+  {
+    name: "Twitter",
+    href: "https://x.com/htmujahid",
+    icon: Twitter,
+    linkTitle: "@htmujahid",
+  },
+]
